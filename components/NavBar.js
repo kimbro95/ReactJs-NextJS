8fbf80f0f9a99110e99f1016aa6f62ecacee053d
bro <gudgudgud714@naver.com>
2022-02-28 14:37:52 +0900

    #1.4 CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,33 +1,32 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
-    console.log(router);
+
     return (
         <nav>
             <Link href="/">
-                <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+                <a
+                    className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}
+                >Home</a>
             </Link>
             <Link href="/about">
-                <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</a>
+                <a className={[
+                    styles.link,
+                    router.pathname === "/about" ? styles.active : "",
+                ].join(" ")}
+                >About</a>
             </Link>
             {/*
-                - Link
-                    페이지 이동시 Single Page App 경험을 제공하려면 Link 컴포넌트를 이용한다
-                    a 태그는 className 이나 style 을 이용할 때 사용하고 href는 사용하지않는다.
-                    Link를 이용하여 href에 경로값을 준다
-                    ex) <a hrdf="/about">About</a>
-                            ↓↓↓↓↓
-                        import Link from 'next/link'
-                        <Link href="/about">
-                            <a>About</a>
-                        </Link>
-                - useRouter
-                    router 객체 내부에 접근하려면 useRouter() 훅을 사용한다.
-                    useRouter는 ReactHook이다. 즉 클래스와 사용할 수 없다.
-                    ex) const router = useRouter()
+                - CSS Module
+                파일명.module.css 생성
+                ex) import styles from "./NavBar.module.css"
+                    className={`${styles.link} ${router.pathname === "/" ? style.active : "" }`}
+                                                OR
+                    className={[styles.link, router.pathname === "/about" ? styles.active : "", ].join(" ")}
             */}
-        </nav>
+        </nav >
     )
 }
