a881be907793e50164c822e220ce85d77acf9d04
bro <gudgudgud714@naver.com>
2022-02-28 14:52:00 +0900

    #1.5 Styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
@@ -8,24 +7,26 @@ export default function NavBar() {
     return (
         <nav>
             <Link href="/">
-                <a
-                    className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}
-                >Home</a>
+                <a className={router.pathname === "/" ? "active" : ""}>Home</a>
             </Link>
             <Link href="/about">
-                <a className={[
-                    styles.link,
-                    router.pathname === "/about" ? styles.active : "",
-                ].join(" ")}
-                >About</a>
+                <a className={router.pathname === "/about" ? "active" : ""}>About</a>
             </Link>
+            <style jsx>{`
+                a{
+                    text-decoration : none;
+                }
+                .active{
+                    color: red;
+                }
+            `}</style>
             {/*
-                - CSS Module
-                파일명.module.css 생성
-                ex) import styles from "./NavBar.module.css"
-                    className={`${styles.link} ${router.pathname === "/" ? style.active : "" }`}
-                                                OR
-                    className={[styles.link, router.pathname === "/about" ? styles.active : "", ].join(" ")}
+                - styles JSX
+                Next.js 사용하면 JavaScript 파일에서 CSS 파일을 가져올 수 있습니다.
+                Next.js가 import 개념을 JS 이상으로 확장했기 떄문에 가능하다.
+                ex) <style jsx></style>
+                스타일 태그에 jsx를 붙여주면된다.
+                다른 부모 컴포넌트에 영향을 주지않고 현재 컴포넌트에만 사용되게해준다.
             */}
         </nav >
     )
